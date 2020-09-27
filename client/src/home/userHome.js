6b9614b390294bb9c18c2488426b8775d484fde6
unknown <jake.yanov@gmail.com>
2020-09-27 17:39:23 +0300

    Commit message

--- client/src/home/userHome.js
+++ client/src/home/userHome.js
@@ -1,54 +1,113 @@
-import React from "react";
-import "./styles.css";
-
-const SearchBox = () => (
-  <div class="input-group mb-3">
-    <input
-      type="text"
-      class="form-control"
-      placeholder="Recipient's username"
-      aria-label="Recipient's username"
-      aria-describedby="button-addon2"
-    />
-    <div class="input-group-append">
-      <button
-        class="btn btn-outline-secondary"
-        type="button"
-        id="button-addon2"
-      >
-        Search
-      </button>
-    </div>
-  </div>
-);
+import React, { useState } from "react";
 
 const UserHome = () => {
+  const [customStyle, setCustomeStyle] = useState("none");
+
+  const w3_open3 = (e) => {
+    console.log(e);
+    console.log(customStyle);
+    if (customStyle === "block") {
+      setCustomeStyle("none");
+    }
+    setCustomeStyle("block");
+  };
+
+  const w3_close = (e) => {
+    console.log(e);
+    console.log(customStyle);
+    setCustomeStyle("none");
+  };
+
+  const MobileHeader = () => (
+    <header className="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+      <div className="w3-bar-item w3-padding-24 w3-wide">LOGO</div>
+      <a
+        className="w3-bar-item w3-button w3-padding-24 w3-right"
+        onClick={(e) => w3_open3(e)}
+      >
+        <i className="fa fa-bars"></i>
+      </a>
+    </header>
+  );
+
+  const Header = () => (
+    <nav
+      className="w3-sidebar w3-bar-block w3-white w3-collapse w3-top"
+      style={{ zIndex: "3", width: "250px", display: customStyle }}
+      id="mySidebar"
+    >
+      <div className="w3-container w3-display-container w3-padding-16">
+        <i
+          onClick={(e) => w3_close(e)}
+          className="fa fa-remove w3-hide-large w3-button w3-display-topright"
+        ></i>
+        <h3 className="w3-wide">
+          <b>LOGO</b>
+        </h3>
+      </div>
+      <div
+        className="w3-padding-64 w3-large w3-text-grey"
+        style={{ fontWeight: "bold", display: customStyle }}
+      >
+        <a href="#" className="w3-bar-item w3-button">
+          Shirts
+        </a>
+        <a href="#" className="w3-bar-item w3-button">
+          Dresses
+        </a>
+
+        <div
+          id="demoAcc"
+          className="w3-bar-block w3-hide w3-padding-large w3-medium"
+        >
+          <a href="#" className="w3-bar-item w3-button w3-light-grey">
+            <i className="fa fa-caret-right w3-margin-right"></i>Skinny
+          </a>
+          <a href="#" className="w3-bar-item w3-button">
+            Relaxed
+          </a>
+          <a href="#" className="w3-bar-item w3-button">
+            Bootcut
+          </a>
+          <a href="#" className="w3-bar-item w3-button">
+            Straight
+          </a>
+        </div>
+        <a href="#" className="w3-bar-item w3-button">
+          Jackets
+        </a>
+        <a href="#" className="w3-bar-item w3-button">
+          Gymwear
+        </a>
+        <a href="#" className="w3-bar-item w3-button">
+          Blazers
+        </a>
+        <a href="#" className="w3-bar-item w3-button">
+          Shoes
+        </a>
+      </div>
+      <a href="#footer" className="w3-bar-item w3-button w3-padding">
+        Contact
+      </a>
+
+      <a href="#footer" className="w3-bar-item w3-button w3-padding">
+        Subscribe
+      </a>
+    </nav>
+  );
+
   return (
     <div>
-      <div className="container1">
-        <img
-          src="https://st2.depositphotos.com/4196725/6840/i/950/depositphotos_68400461-stock-photo-nuts-texture.jpg"
-          alt="Snow"
-          style={{ width: "100%", height: "600px" }}
-        />
-        <div className="bottom-left">Bottom Left</div>
-        <div className="top-left">
-          <img
-            src="https://i.pinimg.com/originals/e1/49/9d/e1499dcf8c44c275f213eca5e9202bf4.jpg"
-            className="logo"
-          />
-        </div>
-        <div className="top-right">Top Right</div>
-        <div className="bottom-right">Bottom Right</div>
-        <div className="centered">
-          <h1>Groceries Delivered in 90 Minute</h1>
-          <h5>
-            Get your healthy foods & snacks delivered at your doorsteps all day
-            everyday
-          </h5>
-          <SearchBox />
-        </div>
-      </div>
+      <Header />
+      <MobileHeader />
+
+      <div
+        class="w3-overlay w3-hide-large"
+        onClick={(e) => w3_close(e)}
+        style={{ cursor: "pointer", display: customStyle }}
+        title="close side menu"
+        id="myOverlay"
+      ></div>
     </div>
   );
 };
